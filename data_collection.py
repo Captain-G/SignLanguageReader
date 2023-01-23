c170d3355fd3d920a2d581b5fc455a9faade7636
Captian-G <poppinpites7@gmail.com>
2023-01-23 18:38:36 +0300

    Added overlay and resize

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
@@ -15,8 +16,26 @@ while True:
         x, y, w, h = hand["bbox"]
 
         img_white = np.ones((image_size, image_size, 3), np.uint8) * 255
-
         img_crop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        img_crop_shape = img_crop.shape
+
+        aspect_ratio = h / w
+        if aspect_ratio > 1:
+            k = image_size / h
+            calculated_w = math.ceil(k * w)
+            img_resize = cv2.resize(img_crop, (calculated_w, image_size))
+            img_resize_shape = img_resize.shape
+            width_gap = math.ceil((image_size - calculated_w) / 2)
+            img_white[:, width_gap:calculated_w + width_gap] = img_resize
+        else:
+            k = image_size / w
+            calculated_h = math.ceil(k * h)
+            img_resize = cv2.resize(img_crop, (image_size, calculated_h))
+            img_resize_shape = img_resize.shape
+            height_gap = math.ceil((image_size - calculated_h) / 2)
+            img_white[height_gap:calculated_h + height_gap, :] = img_resize
+
         cv2.imshow("Image Crop", img_crop)
         cv2.imshow("Image White", img_white)
     cv2.imshow("Image", img)
