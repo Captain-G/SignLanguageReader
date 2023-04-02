9a5187e0c5a5725c2911a008d2fb9b7d8d0ab5f9
Captian-G <poppinpites7@gmail.com>
2023-04-02 16:13:48 +0300

    A sign from above.

--- test_alph.py
+++ test_alph.py
@@ -2,33 +2,32 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
-import time
+from cvzone.ClassificationModule import Classifier
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/alph_keras/keras_model.h5", "Model/alph_keras/labels.txt")
 offset = 20
 image_size = 224
 counter = 0
-
-folder = "Data/TEST"
+labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+          "W", "X", "Y", "Z"]
 
 while True:
     try:
         success, img = cap.read()
-        # cap.read() returns either true or false(stored in success) , and an array of 0-255 stored as img
+        img_output = img.copy()
+        # img_output = img
         hands, img = detector.findHands(img)
-
         if hands:
             hand = hands[0]
-            # hand contains a dictionary with the following  keys : bbox, center etc
             x, y, w, h = hand["bbox"]
-            # x is the starting x co-oridnate, y is the starting y co-ordinate, w is the width, h is the height
+
             img_white = np.ones((image_size, image_size, 3), np.uint8) * 255
-            # np.ones() creates a numpy array of size 224*224*3 of values initialized as 1s(black) *255 white
             img_crop = img[y - offset:y + h + offset, x - offset:x + w + offset]
-            # crops the img from the webcam footage to only the hand
+
             img_crop_shape = img_crop.shape
-            # img_crop_shape returns dimensions of the cropped image eg 255, 204,3
+
             aspect_ratio = w / h
             if aspect_ratio > 1:
                 k = image_size / w
@@ -37,21 +36,28 @@ while True:
                 img_resize_shape = img_resize.shape
                 height_gap = math.ceil((image_size - calculated_h) / 2)
                 img_white[height_gap:calculated_h + height_gap, :] = img_resize
+                prediction, index = classifier.getPrediction(img_white, draw=False)
+                # prediction, index = classifier.getPrediction(img_white)
+
             else:
+
                 k = image_size / h
                 calculated_w = math.ceil(k * w)
                 img_resize = cv2.resize(img_crop, (calculated_w, image_size))
                 img_resize_shape = img_resize.shape
                 width_gap = math.ceil((image_size - calculated_w) / 2)
                 img_white[:, width_gap:calculated_w + width_gap] = img_resize
+                prediction, index = classifier.getPrediction(img_white, draw=False)
+                # prediction, index = classifier.getPrediction(img_white)
 
-            cv2.imshow("Image Crop", img_crop)
-            cv2.imshow("Image White", img_white)
-        cv2.imshow("Image", img)
-        key = cv2.waitKey(1)
-        if key == ord("s"):
-            counter += 1
-            cv2.imwrite(f"{folder}/Image_{time.time()}.jpg", img_white)
-            print(counter)
+            cv2.rectangle(img_output, (x - offset, y - offset - 50), (x - offset + 90, y - offset), (255, 0, 255),
+                          cv2.FILLED)
+            cv2.putText(img_output, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
+            cv2.rectangle(img_output, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
+
+            # cv2.imshow("Image Crop", img_crop)
+            # cv2.imshow("Image White", img_white)
+        cv2.imshow("Image", img_output)
+        cv2.waitKey(1)
     except Exception as e:
         print(e)
--- test_numbers.py
+++ test_numbers.py
@@ -2,54 +2,55 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
-import time
 from cvzone.ClassificationModule import Classifier
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier("NumberModel/keras_model.h5", "Model/labels.txt")
+classifier = Classifier("Model/num_keras/keras_model.h5", "Model/num_keras/labels.txt")
 offset = 20
-image_size = 300
+image_size = 224
 counter = 0
 labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 while True:
-    success, img = cap.read()
-    img_output = img.copy()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand["bbox"]
+    try:
+        success, img = cap.read()
+        img_output = img.copy()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand["bbox"]
 
-        img_white = np.ones((image_size, image_size, 3), np.uint8) * 255
-        img_crop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+            img_white = np.ones((image_size, image_size, 3), np.uint8) * 255
+            img_crop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        img_crop_shape = img_crop.shape
+            img_crop_shape = img_crop.shape
 
-        aspect_ratio = h / w
-        if aspect_ratio > 1:
-            k = image_size / h
-            calculated_w = math.ceil(k * w)
-            img_resize = cv2.resize(img_crop, (calculated_w, image_size))
-            img_resize_shape = img_resize.shape
-            width_gap = math.ceil((image_size - calculated_w) / 2)
-            img_white[:, width_gap:calculated_w + width_gap] = img_resize
-            prediction, index = classifier.getPrediction(img_white, draw=False)
+            aspect_ratio = w / h
+            if aspect_ratio > 1:
+                k = image_size / w
+                calculated_h = math.ceil(k * h)
+                img_resize = cv2.resize(img_crop, (image_size, calculated_h))
+                img_resize_shape = img_resize.shape
+                height_gap = math.ceil((image_size - calculated_h) / 2)
+                img_white[height_gap:calculated_h + height_gap, :] = img_resize
+                prediction, index = classifier.getPrediction(img_white, draw=False)
+            else:
+                k = image_size / h
+                calculated_w = math.ceil(k * w)
+                img_resize = cv2.resize(img_crop, (calculated_w, image_size))
+                img_resize_shape = img_resize.shape
+                width_gap = math.ceil((image_size - calculated_w) / 2)
+                img_white[:, width_gap:calculated_w + width_gap] = img_resize
+                prediction, index = classifier.getPrediction(img_white, draw=False)
 
-        else:
-            k = image_size / w
-            calculated_h = math.ceil(k * h)
-            img_resize = cv2.resize(img_crop, (image_size, calculated_h))
-            img_resize_shape = img_resize.shape
-            height_gap = math.ceil((image_size - calculated_h) / 2)
-            img_white[height_gap:calculated_h + height_gap, :] = img_resize
-            prediction, index = classifier.getPrediction(img_white, draw=False)
-
-        cv2.rectangle(img_output, (x - offset, y - offset - 50), (x - offset + 90, y - offset), (255, 0, 255),
-                      cv2.FILLED)
-        cv2.putText(img_output, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
-        cv2.rectangle(img_output, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
-        cv2.imshow("Image Crop", img_crop)
-        cv2.imshow("Image White", img_white)
-    cv2.imshow("Image", img_output)
-    cv2.waitKey(1)
+            cv2.rectangle(img_output, (x - offset, y - offset - 50), (x - offset + 90, y - offset), (255, 0, 255),
+                          cv2.FILLED)
+            cv2.putText(img_output, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
+            cv2.rectangle(img_output, (x - offset, y - offset), (x + w + offset, y + h + offset), (255, 0, 255), 4)
+            # cv2.imshow("Image Crop", img_crop)
+            # cv2.imshow("Image White", img_white)
+        cv2.imshow("Image", img_output)
+        cv2.waitKey(1)
+    except Exception as e:
+        print(e)
